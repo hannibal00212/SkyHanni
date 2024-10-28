98f0a822f210527ae13e87ef4a46568782309dc1
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-10-28 18:51:46 +0100

    improve decomposing
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/utils/SpecialColor.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/SpecialColor.kt
@@ -15,16 +15,16 @@ object SpecialColor {
     fun specialToChromaRGB(special: String): Int {
         if (startTime.isFarPast()) startTime = SimpleTimeMark.now()
 
-        val (b, g, r, a, chroma) = decompose(special)
-        val (hue, sat, bri) = Color.RGBtoHSB(r, g, b, null)
+        val (chroma, alpha, red, green, blue) = decompose(special)
+        val (hue, sat, bri) = Color.RGBtoHSB(red, green, blue, null)
 
         val adjustedHue = if (chroma > 0) (hue + (startTime.passedSince().inWholeMilliseconds / 1000f / chromaSpeed(chroma) % 1)).let {
             if (it < 0) it + 1f else it
         } else hue
 
-        return (a and 0xFF) shl 24 or (Color.HSBtoRGB(adjustedHue, sat, bri) and 0x00FFFFFF)
+        return (alpha and 0xFF) shl 24 or (Color.HSBtoRGB(adjustedHue, sat, bri) and 0x00FFFFFF)
     }
 
-    private fun decompose(csv: String) = csv.split(":").mapNotNull { it.toIntOrNull() }.reversed().toIntArray()
+    private fun decompose(csv: String) = csv.split(":").mapNotNull { it.toIntOrNull() }.toIntArray()
     private fun chromaSpeed(speed: Int) = (255 - speed) / 254f * (MAX_CHROMA_SECS - MIN_CHROMA_SECS) + MIN_CHROMA_SECS
 }
