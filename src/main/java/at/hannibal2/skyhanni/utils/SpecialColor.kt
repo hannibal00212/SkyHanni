7dffd9b70de3b76f89ec0a40b2fa6af890e1be21
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-10-28 18:30:58 +0100

    fix colors
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/utils/SpecialColor.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/SpecialColor.kt
@@ -16,7 +16,7 @@ object SpecialColor {
     fun specialToChromaRGB(special: String): Int {
         if (startTime < 0) startTime = System.currentTimeMillis()
 
-        val (r, g, b, a, chroma) = decompose(special)
+        val (b, g, r, a, chroma) = decompose(special)
         val (hue, sat, bri) = Color.RGBtoHSB(r, g, b, null)
 
         val adjustedHue = if (chroma > 0) (hue + ((System.currentTimeMillis() - startTime) / 1000f / chromaSpeed(chroma) % 1)).let {
@@ -26,6 +26,6 @@ object SpecialColor {
         return (a and 0xFF) shl 24 or (Color.HSBtoRGB(adjustedHue, sat, bri) and 0x00FFFFFF)
     }
 
-    private fun decompose(csv: String) = csv.split(":").mapNotNull { it.toIntOrNull(RADIX) }.toIntArray()
+    private fun decompose(csv: String) = csv.split(":").mapNotNull { it.toIntOrNull(RADIX) }.reversed().toIntArray()
     private fun chromaSpeed(speed: Int) = (255 - speed) / 254f * (MAX_CHROMA_SECS - MIN_CHROMA_SECS) + MIN_CHROMA_SECS
 }
