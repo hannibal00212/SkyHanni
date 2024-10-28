d2f35590b4f50ccc4228395b66479c4ec07dc72d
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-10-28 18:47:27 +0100

    default radix is alr 10
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/utils/SpecialColor.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/SpecialColor.kt
@@ -7,7 +7,6 @@ import java.awt.Color
  * translated to Kotlin and slightly modified.
  */
 object SpecialColor {
-    private const val RADIX = 10
     private const val MIN_CHROMA_SECS = 1
     private const val MAX_CHROMA_SECS = 60
     private var startTime = SimpleTimeMark.farPast()
@@ -26,6 +25,6 @@ object SpecialColor {
         return (a and 0xFF) shl 24 or (Color.HSBtoRGB(adjustedHue, sat, bri) and 0x00FFFFFF)
     }
 
-    private fun decompose(csv: String) = csv.split(":").mapNotNull { it.toIntOrNull(RADIX) }.reversed().toIntArray()
+    private fun decompose(csv: String) = csv.split(":").mapNotNull { it.toIntOrNull() }.reversed().toIntArray()
     private fun chromaSpeed(speed: Int) = (255 - speed) / 254f * (MAX_CHROMA_SECS - MIN_CHROMA_SECS) + MIN_CHROMA_SECS
 }
