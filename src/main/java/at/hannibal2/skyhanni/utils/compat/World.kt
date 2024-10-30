b5097746727dd499ddfc3c897a971cd71cd74d23
Cal <cwolfson58@gmail.com>
2024-10-30 12:00:32 +1100

    move to seperate file

--- src/main/java/at/hannibal2/skyhanni/utils/compat/World.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/compat/World.kt
@@ -6,18 +6,6 @@ import net.minecraft.entity.Entity
 import net.minecraft.entity.item.EntityArmorStand
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.potion.Potion
-import net.minecraft.util.IChatComponent
-//#if MC > 1.12
-//$$ import net.minecraft.init.PotionTypes
-//#endif
-//#if MC > 1.16
-//$$ import net.minecraft.ChatFormatting
-//$$ import net.minecraft.network.chat.TextColor
-//#endif
-//#if MC > 1.20
-//$$ import net.minecraft.text.MutableText
-//$$ import net.minecraft.text.PlainTextContent
-//#endif
 
 fun WorldClient.getLoadedPlayers(): List<EntityPlayer> =
 //#if MC < 1.14
@@ -44,48 +32,6 @@ fun Minecraft.isOnMainThread() =
     this.isCallingFromMinecraftThread
 //#else
 //$$ this.isSameThread
-//#endif
-
-fun IChatComponent.getDirectlyContainedText() =
-//#if MC < 1.16
-    this.unformattedTextForChat
-//#elseif MC < 1.20
-//$$    this.contents
-//#else
-//$$        (this.content as? PlainTextContent)?.string() ?: ""
-//#endif
-
-fun IChatComponent.getFormattedTextCompat() =
-//#if MC < 1.16
-    this.formattedText
-//#else
-//$$run {
-//$$    val sb = StringBuilder()
-//$$    for (component in iterator()) {
-//$$        sb.append(component.style.color?.toChatFormatting()?.toString() ?: "§r")
-//$$        sb.append(component.getDirectlyContainedText())
-//$$        sb.append("§r")
-//$$    }
-//$$    sb.toString()
-//$$}
-//$$
-//$$private val textColorLUT = ChatFormatting.entries
-//$$    .mapNotNull { formatting -> formatting.color?.let { it to formatting } }
-//$$    .toMap()
-//$$
-//$$fun TextColor.toChatFormatting(): ChatFormatting? {
-//$$    return textColorLUT[this.value]
-//$$}
-//$$
-//$$fun Component.iterator(): Sequence<Component> {
-//$$    return sequenceOf(this) + siblings.asSequence().flatMap { it.iterator() } // TODO: in theory we want to properly inherit styles here
-//$$}
-//#endif
-
-//#if MC > 1.20
-//$$fun MutableText.withColor(formatting: Formatting): Text {
-//$$    return this.styled { it.withColor(formatting) }
-//$$}
 //#endif
 
 object Effects {
