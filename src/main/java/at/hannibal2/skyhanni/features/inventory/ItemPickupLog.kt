6558defbbbfd3185f9b9b62a76fa83a2b112cf21
catgirlseraid <77941535+catgirlseraid@users.noreply.github.com>
2024-11-01 23:03:24 +1300

    detekt grrr

--- src/main/java/at/hannibal2/skyhanni/features/inventory/ItemPickupLog.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/ItemPickupLog.kt
@@ -123,7 +123,7 @@ object ItemPickupLog {
 
         event.sackChanges.forEach {
             var itemStack = (it.internalName.getItemStack())
-            //TODO this should not need to be done here but the whole internal name resolving needs a rework and this fixes it for now
+            // TODO this should not need to be done here but the whole internal name resolving needs a rework and this fixes it for now
             if (it.internalName == "HAY_BALE".asInternalName()) itemStack = "HAY_BLOCK".asInternalName().getItemStack()
 
             val item = PickupEntry(itemStack.dynamicName(), it.delta.absoluteValue.toLong(), it.internalName)
