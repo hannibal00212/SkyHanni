b687cf3aa1fbee0f883f5151cec1a3249ac04c3c
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-11-01 21:02:02 +0100

    moved precondition to init block
    Co-authored-by: Empa <itsempa@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/utils/CircularList.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/CircularList.kt
@@ -4,10 +4,13 @@ class CircularList<T>(private val items: List<T>) {
 
     constructor(vararg elements: T) : this(elements.asList())
 
+    init {
+        require(items.isNotEmpty()) { "CircularList must not be empty" }
+    }
+
     private var index = 0
 
     fun next(): T {
-        if (items.isEmpty()) throw NoSuchElementException("CircularList is empty")
         val item = items[index]
         index = (index + 1) % items.size // Increment index and wrap around
         return item
