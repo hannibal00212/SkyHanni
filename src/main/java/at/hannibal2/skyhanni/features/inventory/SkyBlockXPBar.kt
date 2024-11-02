dc39dad860e1cbb464fdc6303dbf96ccbdbbb9c6
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-11-02 16:03:03 +0100

    display the xp in the bar
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/inventory/SkyBlockXPBar.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/SkyBlockXPBar.kt
@@ -1,7 +1,7 @@
 package at.hannibal2.skyhanni.features.inventory
 
 import at.hannibal2.skyhanni.SkyHanniMod
-import at.hannibal2.skyhanni.events.LorenzTickEvent
+import at.hannibal2.skyhanni.api.SkyBlockXPAPI
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.LorenzUtils
 import net.minecraft.client.Minecraft
@@ -11,18 +11,13 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 @SkyHanniModule
 object SkyBlockXPBar {
 
-    var xp = 0f
-
-    @SubscribeEvent
-    fun onTick(event: LorenzTickEvent) {
-        xp = Math.random().toFloat()
-    }
-
     @SubscribeEvent
     fun onRenderScoreboard(event: RenderGameOverlayEvent.Pre) {
         if (!isEnabled()) return
         if (event.type != RenderGameOverlayEvent.ElementType.EXPERIENCE) return
-        Minecraft.getMinecraft().thePlayer.setXPStats(xp, 100, 69)
+        val xp = SkyBlockXPAPI.xp ?: return
+        val level = SkyBlockXPAPI.level ?: return
+        Minecraft.getMinecraft().thePlayer.setXPStats(xp / 100f, 100, level)
     }
 
     private fun isEnabled() = LorenzUtils.inSkyBlock && SkyHanniMod.Companion.feature.inventory.skyblockXpBar
