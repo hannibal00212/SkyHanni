41f8c872e7dfd9753b04130f71cb5f9f0a77f8ff
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-11-05 23:28:42 +0100

    add copper
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/elements/ScoreboardElementCopper.kt
+++ src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/elements/ScoreboardElementCopper.kt
@@ -3,19 +3,26 @@ package at.hannibal2.skyhanni.features.gui.customscoreboard.elements
 import at.hannibal2.skyhanni.features.garden.GardenAPI
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboard.displayConfig
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboard.informationFilteringConfig
+import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboardNumberTrackingElement
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboardUtils.formatStringNum
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboardUtils.getCopper
 
 // scoreboard
 // scoreboard update event
-object ScoreboardElementCopper : ScoreboardElement() {
+object ScoreboardElementCopper : ScoreboardElement(), CustomScoreboardNumberTrackingElement {
+    override var previousAmount: Long = 0
+    override var temporaryChangeDisplay: String? = null
+    override val numberColor = "§c"
+
     override fun getDisplay(): String? {
-        val copper = formatStringNum(getCopper())
+        val copper = getCopper()
+        checkDifference(copper.toLong())
+        val line = formatStringNum(copper) + (temporaryChangeDisplay ?: "")
 
         return when {
-            informationFilteringConfig.hideEmptyLines && copper == "0" -> null
-            displayConfig.displayNumbersFirst -> "§c$copper Copper"
-            else -> "Copper: §c$copper"
+            informationFilteringConfig.hideEmptyLines && line == "0" -> null
+            displayConfig.displayNumbersFirst -> "§c$line Copper"
+            else -> "Copper: §c$line"
         }
     }
 
