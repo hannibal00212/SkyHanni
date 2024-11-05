5910384ee314e1a987402b7cf998753b1228305a
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-11-05 23:30:34 +0100

    add motes
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/elements/ScoreboardElementMotes.kt
+++ src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/elements/ScoreboardElementMotes.kt
@@ -2,20 +2,27 @@ package at.hannibal2.skyhanni.features.gui.customscoreboard.elements
 
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboard.displayConfig
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboard.informationFilteringConfig
+import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboardNumberTrackingElement
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboardUtils.formatStringNum
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboardUtils.getMotes
 import at.hannibal2.skyhanni.features.rift.RiftAPI
 
 // scoreboard
 // scoreboard update event
-object ScoreboardElementMotes : ScoreboardElement() {
+object ScoreboardElementMotes : ScoreboardElement(), CustomScoreboardNumberTrackingElement {
+    override var previousAmount: Long = 0
+    override var temporaryChangeDisplay: String? = null
+    override val numberColor = "§d"
+
     override fun getDisplay(): String? {
-        val motes = formatStringNum(getMotes())
+        val motes = getMotes()
+        checkDifference(motes.toLong())
+        val line = formatStringNum(motes) + (ScoreboardElementCopper.temporaryChangeDisplay ?: "")
 
         return when {
-            informationFilteringConfig.hideEmptyLines && motes == "0" -> null
-            displayConfig.displayNumbersFirst -> "§d$motes Motes"
-            else -> "Motes: §d$motes"
+            informationFilteringConfig.hideEmptyLines && line == "0" -> null
+            displayConfig.displayNumbersFirst -> "§d$line Motes"
+            else -> "Motes: §d$line"
         }
     }
 
