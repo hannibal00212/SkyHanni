62bee6de6595be6529fc8b639abe1c30d5bb3e66
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-11-05 23:32:48 +0100

    add soulflow
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/elements/ScoreboardElementSoulflow.kt
+++ src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/elements/ScoreboardElementSoulflow.kt
@@ -2,18 +2,27 @@ package at.hannibal2.skyhanni.features.gui.customscoreboard.elements
 
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboard.displayConfig
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboard.informationFilteringConfig
+import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboardNumberTrackingElement
+import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboardUtils.formatStringNum
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboardUtils.getSoulflow
 import at.hannibal2.skyhanni.features.rift.RiftAPI
 
 // widget
 // widget update event
-object ScoreboardElementSoulflow : ScoreboardElement() {
+object ScoreboardElementSoulflow : ScoreboardElement(), CustomScoreboardNumberTrackingElement {
+    override var previousAmount: Long = 0
+    override var temporaryChangeDisplay: String? = null
+    override val numberColor = "§3"
+
     override fun getDisplay(): String? {
         val soulflow = getSoulflow()
+        checkDifference(soulflow.toLong())
+        val line = formatStringNum(soulflow) + (temporaryChangeDisplay ?: "")
+
         return when {
-            informationFilteringConfig.hideEmptyLines && soulflow == "0" -> null
-            displayConfig.displayNumbersFirst -> "§3$soulflow Soulflow"
-            else -> "Soulflow: §3$soulflow"
+            informationFilteringConfig.hideEmptyLines && line == "0" -> null
+            displayConfig.displayNumbersFirst -> "§3$line Soulflow"
+            else -> "Soulflow: §3$line"
         }
     }
 
