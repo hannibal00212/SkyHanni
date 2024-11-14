bf5a7d6b03528b8596222fd0369d5835c4e9cbe5
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-11-14 15:07:51 +0100

    fix
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/ScoreboardPattern.kt
+++ src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/ScoreboardPattern.kt
@@ -433,7 +433,7 @@ object ScoreboardPattern {
      */
     val thirdObjectiveLinePattern by miscSb.pattern(
         "thirdobjectiveline",
-        "\\s*§.\\(§.\\w+§.\\/§.\\w+§.\\)|§f Mages.*|§f Barbarians.*|§edefeat Kuudra|§eand stun him|§.Fish \\d .*[fF]ish §.[✖✔]",
+        "§eProtect Elle §7\\(§.\\d+%§7\\)|\\s*§.\\(§.\\w+§.\\/§.\\w+§.\\)|§f Mages.*|§f Barbarians.*|§edefeat Kuudra|§eand stun him|§.Fish \\d .*[fF]ish §.[✖✔]",
     )
 
     // collection of lines that just randomly exist and I have no clue how on earth to effectively remove them
