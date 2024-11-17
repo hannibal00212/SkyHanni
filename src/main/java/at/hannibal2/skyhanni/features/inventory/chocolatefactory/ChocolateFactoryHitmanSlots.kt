fdfee86e5b123aa5bbc0a0aa776447300350607a
David Cole <40234707+DavidArthurCole@users.noreply.github.com>
2024-11-17 00:10:42 -0500

    Progress is slow

--- src/main/java/at/hannibal2/skyhanni/features/inventory/chocolatefactory/ChocolateFactoryHitmanSlots.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/chocolatefactory/ChocolateFactoryHitmanSlots.kt
@@ -1,6 +1,7 @@
 package at.hannibal2.skyhanni.features.inventory.chocolatefactory
 
 import at.hannibal2.skyhanni.api.event.HandleEvent
+import at.hannibal2.skyhanni.events.InventoryCloseEvent
 import at.hannibal2.skyhanni.events.InventoryFullyOpenedEvent
 import at.hannibal2.skyhanni.events.LorenzTickEvent
 import at.hannibal2.skyhanni.events.hoppity.RabbitFoundEvent
@@ -10,14 +11,9 @@ import at.hannibal2.skyhanni.features.event.hoppity.HoppityEggType
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ItemUtils.getLore
 import at.hannibal2.skyhanni.utils.ItemUtils.setLore
-import at.hannibal2.skyhanni.utils.RegexUtils.firstMatcher
 import at.hannibal2.skyhanni.utils.RegexUtils.matches
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.hours
-import kotlin.time.Duration.Companion.minutes
-import kotlin.time.Duration.Companion.seconds
 
 @SkyHanniModule
 object ChocolateFactoryHitmanSlots {
@@ -45,29 +41,14 @@ object ChocolateFactoryHitmanSlots {
     private val config get() = ChocolateFactoryAPI.config
     private val hitmanRabbits = mutableListOf<HitmanRabbit>()
 
-    private var slotToHitmanMap = mutableMapOf<Int, HitmanRabbit>()
+    private var cooldownSlotIndices = emptySet<Int>()
 
     data class HitmanRabbit (
         val rabbitName: String,
         val claimedAt: SimpleTimeMark,
         var expiresAt: SimpleTimeMark? = null,
+        var claimedBySlot: Boolean = false
     )
-
-    /**
-     * Given a string in the format of "8h 38m 6s", parse it into a [SimpleTimeMark].
-     */
-    private fun String.parseCooldownToTimeMark(): SimpleTimeMark {
-        val time = this.split(" ").mapNotNull {
-            when {
-                it.endsWith("h") -> it.dropLast(1).toLongOrNull()?.hours
-                it.endsWith("m") -> it.dropLast(1).toLongOrNull()?.minutes
-                it.endsWith("s") -> it.dropLast(1).toLongOrNull()?.seconds
-                else -> null
-            }
-        }.reduceOrNull { acc, duration -> acc + duration } ?: Duration.ZERO
-
-        return SimpleTimeMark.now().plus(time)
-    }
 
     @HandleEvent
     fun onRabbitFound(event: RabbitFoundEvent) {
@@ -81,11 +62,20 @@ object ChocolateFactoryHitmanSlots {
     }
 
     @SubscribeEvent
+    fun onInventoryClose(event: InventoryCloseEvent) {
+        // Free all slots when the inventory is closed
+        hitmanRabbits.forEach { it.claimedBySlot = false }
+    }
+
+    @SubscribeEvent
     fun replaceItem(event: ReplaceItemEvent) {
         if (!config.hitmanSlotInfo) return
         if (!hitmanInventoryPattern.matches(event.inventory.name)) return
+        if (!cooldownSlotIndices.contains(event.slot)) return
 
-        val hitmanRabbit = slotToHitmanMap[event.slot] ?: return
+        val hitmanRabbit = hitmanRabbits.sortedBy { it.claimedAt }.firstOrNull { !it.claimedBySlot }
+            ?: return
+        hitmanRabbit.claimedBySlot = true
         val originalItemStack = event.originalItem
         val lore = originalItemStack.getLore().toMutableList()
         /**
@@ -111,38 +101,8 @@ object ChocolateFactoryHitmanSlots {
         if (!hitmanInventoryPattern.matches(event.inventoryName)) return
         if (hitmanRabbits.isEmpty()) return
 
-        val cooldownSlots = event.inventoryItems.filterValues {
+        cooldownSlotIndices = event.inventoryItems.filterValues {
             it.hasDisplayName() && slotOnCooldownPattern.matches(it.displayName)
-        }
-
-        // Remove the first [claimedSlotCount] slots from the cooldown slots
-        var claimedSlotCount = hitmanRabbits.count { it.expiresAt != null }
-        var claimedSlotKeys = cooldownSlots.keys.sorted().take(claimedSlotCount).toSet()
-        var unclaimedSlotKeys = cooldownSlots.keys - claimedSlotKeys
-
-        // Ensure that unclaimedSlotKeys is the same size as the list of uninitialized hitman rabbits
-        // If not we need to pop that discrepancy from the hitmanRabbits list
-        val nonProcessedRabbits = hitmanRabbits.filter { it.expiresAt == null }
-        while(unclaimedSlotKeys.size < nonProcessedRabbits.size) {
-            // Remove the rabbit that expires soonest
-            hitmanRabbits.filter { it.expiresAt != null }.minByOrNull { it.expiresAt!! }?.let {
-                hitmanRabbits.remove(it)
-                claimedSlotCount--
-                claimedSlotKeys = cooldownSlots.keys.sorted().take(claimedSlotCount).toSet()
-                unclaimedSlotKeys = cooldownSlots.keys - claimedSlotKeys
-            } ?: break
-        }
-
-        // Assign the expiration time to the uninitialized hitman rabbits
-        unclaimedSlotKeys.forEach { slotKey ->
-            val slot = cooldownSlots[slotKey] ?: return@forEach
-            cooldownPattern.firstMatcher(slot.getLore()) {
-                val cooldown = group("cooldown").parseCooldownToTimeMark()
-                // Get the hitman rabbit that has the oldest claimedAt time, and is not yet assigned an expiration time
-                val hitmanRabbit = hitmanRabbits.filter { it.expiresAt == null }.minByOrNull { it.claimedAt } ?: return@firstMatcher
-                hitmanRabbit.expiresAt = cooldown
-                slotToHitmanMap[slotKey] = hitmanRabbit
-            }
-        }
+        }.keys
     }
 }
