f2aeacde5d6de899bf8c036962a40a4398963308
Empa <itsempa@users.noreply.github.com>
2024-11-22 13:42:30 +0100

    change to val
    Signed-off-by: Empa <itsempa@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/inventory/wardrobe/CustomWardrobe.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/wardrobe/CustomWardrobe.kt
@@ -52,9 +52,9 @@ object CustomWardrobe {
     private var editMode = false
     private var waitingForInventoryUpdate = false
 
-    private var position: Position = Position(0, 0).ignoreScale()
-    private var loadingPosition: Position = Position(0, 0).ignoreScale()
-    private var inventoryButtonPosition: Position = Position(0, 0).ignoreScale()
+    private val position: Position = Position(0, 0).ignoreScale()
+    private val loadingPosition: Position = Position(0, 0).ignoreScale()
+    private val inventoryButtonPosition: Position = Position(0, 0).ignoreScale()
 
     private var activeScale: Int = 100
     private var currentMaxSize: Pair<Int, Int>? = null
