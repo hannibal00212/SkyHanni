89f0675f850e00740cad086f28548726fb1529b2
Empa <itsempa@users.noreply.github.com>
2024-11-29 08:28:53 +0100

    make TimeLimitedSet implement MutableSet
    Signed-off-by: Empa <itsempa@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/utils/TimeLimitedSet.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/TimeLimitedSet.kt
@@ -6,36 +6,66 @@ import kotlin.time.Duration
 @Suppress("UnstableApiUsage")
 class TimeLimitedSet<T : Any>(
     expireAfterWrite: Duration,
-    private val removalListener: (T, RemovalCause) -> Unit = { _, _ -> },
-) : MutableIterable<T> {
+    private val removalListener: ((T?, RemovalCause) -> Unit)? = null,
+) : MutableSet<T> {
 
     private val cache = TimeLimitedCache<T, Unit>(expireAfterWrite) { key, _, cause ->
-        key?.let {
-            removalListener(it, cause)
+        removalListener?.let {
+            it(key, cause)
         }
     }
 
-    fun add(element: T) {
-        cache[element] = Unit
+    override val size: Int get() = cache.size
+
+    override fun isEmpty(): Boolean = cache.isEmpty()
+
+    override operator fun contains(element: T): Boolean = cache.containsKey(element)
+
+    override fun add(element: T): Boolean {
+        return (element in cache).also { cache[element] = Unit }
     }
 
-    operator fun plusAssign(element: T) = add(element)
+    override fun remove(element: T): Boolean {
+        if (element !in cache) return false
+        cache.remove(element)
+        return true
+    }
 
     fun addIfAbsent(element: T) {
         if (!contains(element)) add(element)
     }
 
-    fun remove(element: T) = cache.remove(element)
-
-    operator fun minusAssign(element: T) {
-        remove(element)
+    override fun addAll(elements: Collection<T>): Boolean {
+        var value = false
+        for (element in elements) {
+            if (add(element)) value = true
+        }
+        return value
     }
 
-    operator fun contains(element: T): Boolean = cache.containsKey(element)
+    override fun clear() = cache.clear()
 
-    fun clear() = cache.clear()
+    override fun containsAll(elements: Collection<T>): Boolean {
+        return elements.all { it in cache }
+    }
 
-    fun toSet(): MutableSet<T> = cache.keys
+    override fun iterator(): MutableIterator<T> = cache.keys.iterator()
+    override fun retainAll(elements: Collection<T>): Boolean {
+        var value = false
+        for (key in cache.keys) {
+            if (key !in elements) {
+                remove(key)
+                value = true
+            }
+        }
+        return value
+    }
 
-    override fun iterator(): MutableIterator<T> = toSet().iterator()
+    override fun removeAll(elements: Collection<T>): Boolean {
+        var value = false
+        for (element in elements) {
+            if (remove(element)) value = true
+        }
+        return value
+    }
 }
