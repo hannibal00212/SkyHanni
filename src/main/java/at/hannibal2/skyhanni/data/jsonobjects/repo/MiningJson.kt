f5738ee7fcb83967427316baed7dcd36f1deb19f
Thunderblade73 <gaidermarkus@gmail.com>
2024-12-04 11:36:13 +0100

    rename of json

--- src/main/java/at/hannibal2/skyhanni/data/jsonobjects/repo/MiningJson.kt
+++ src/main/java/at/hannibal2/skyhanni/data/jsonobjects/repo/MiningJson.kt
@@ -1,7 +1,8 @@
 package at.hannibal2.skyhanni.data.jsonobjects.repo
 
 import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
 
 data class MiningJson(
-    @Expose val blockStrengths: Map<String, Int>,
+    @Expose @SerializedName("block_strengths") val blockStrengths: Map<String, Int>,
 )
