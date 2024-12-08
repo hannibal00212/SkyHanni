ee8dd51263183b162383b8b96c3f4d1149e6c3d4
aphased <152172867+aphased@users.noreply.github.com>
2024-12-08 02:04:40 +0100

    Use null over -1 & combine functionality where same
    This drastically reduces repetitiveness.
    Also, crucially, fixed not updating the last stored _type_ of storage (backpack vs. ender chest), only the page number/index; and this time in the correct location.
    Also trimmed use of `event.message.startsWith(x, ignoreCase = true)` where possible.

--- src/main/java/at/hannibal2/skyhanni/features/commands/OpenLastStorage.kt
+++ src/main/java/at/hannibal2/skyhanni/features/commands/OpenLastStorage.kt
@@ -13,113 +13,94 @@ object OpenLastStorage {
         ENDER_CHEST, BACKPACK
     }
 
-    private var lastBackpack = -1
-    private var lastEnderChest = -1
+    private var lastBackpack: Int? = null
+    private var lastEnderChest: Int? = null
     // Default to Ender Chest as last storage type, since every profile on any
     // account has at least one partial ender chest page unlocked
     private var lastStorageType = StorageType.ENDER_CHEST
 
-    private fun openLastBackPack() {
-        HypixelCommands.openBackpack(lastBackpack)
-        ChatUtils.chat("Opened last backpack $lastBackpack.")
-    }
+    private fun openLastStorage(storageType: StorageType) {
+        val storageItem = when (storageType) {
+            StorageType.BACKPACK -> lastBackpack.also { it?.let { HypixelCommands.openBackpack(it) } }
+            StorageType.ENDER_CHEST -> lastEnderChest.also { it?.let { HypixelCommands.openEnderChest(it) } }
+        }
 
-    private fun openLastEnderChest() {
-        HypixelCommands.openEnderChest(lastEnderChest)
-        ChatUtils.chat("Opened last ender chest $lastEnderChest.")
+        val storageMessage = storageItem?.let { "Opened last ${storageType.name.lowercase().replace("_", " ")} $it." }
+            ?: "No last ${storageType.name.lowercase().replace("_", " ")} to open."
+
+        ChatUtils.chat(storageMessage)
     }
 
     @SubscribeEvent
     fun onMessageSendToServer(event: MessageSendToServerEvent) {
         if (event.senderIsSkyhanni()) return
 
-        if (handleBackpack(event)) return
-        if (handleEnderChest(event)) return
+        val message = event.message.lowercase()
 
-        // Non-Hypixel combined "last opened" command for backpack + ender chest
-        if (!event.message.startsWith("/lastopened", ignoreCase = true) &&
-            // cannot use startsWith because of /locraw
-            !event.message.equals("/lo", ignoreCase = true)
+        if (message.startsWith("/backpack") ||
+            message.startsWith("/bp")
+        ) {
+            handleStorage(event, message, StorageType.BACKPACK)
+        } else if (message.startsWith("/enderchest") ||
+                   message.startsWith("/ec")
+        ) {
+            handleStorage(event, message, StorageType.ENDER_CHEST)
+        }
+
+        // Non-Hypixel combined "last opened" command for backpack + ender chest.
+        // Cannot use startsWith for the shorthand-form command because of /locraw:
+        if (!message.startsWith("/lastopened") && message != "/lo"
         ) {
             return
         }
-
         event.isCanceled = true
-
-        if (lastStorageType == StorageType.ENDER_CHEST) {
-            openLastEnderChest()
-        } else if (lastStorageType == StorageType.BACKPACK) {
-            openLastBackPack()
-        }
+        openLastStorage(lastStorageType)
     }
 
-    private fun handleBackpack(event: MessageSendToServerEvent): Boolean {
-        if (!event.message.startsWith("/backpack", ignoreCase = true) &&
-            !event.message.startsWith("/bp", ignoreCase = true)
-        ) {
-            return false
+    private fun handleStorage(event: MessageSendToServerEvent, message: String, storageType: StorageType) {
+        val commandPrefix = when (storageType) {
+            StorageType.BACKPACK -> arrayOf("/backpack", "/bp")
+            StorageType.ENDER_CHEST -> arrayOf("/enderchest", "/ec")
         }
 
-        if (event.message.startsWith("/backpack -", ignoreCase = true) ||
-            event.message.startsWith("/bp -", ignoreCase = true)
-        ) {
-            event.isCanceled = true
-            if (lastBackpack == -1) {
-                ChatUtils.chat("There is no previous backpack to reopen!")
-                return true
-            }
-            openLastBackPack()
-            lastStorageType = StorageType.BACKPACK
-            return true
+        if (!commandPrefix.any { message.startsWith(it) }) {
+            return
         }
 
-        val parts = event.message.split(" ")
-        if (parts.size <= 1) {
-            // "/bp" without any argument opens backpack #1
-            lastBackpack = 1
-            return true
-        }
-        val intArg = parts[1].toIntOrNull()
-        if (intArg != null) {
-            // "/bp 0" still is a valid command (leads to the overview menu)
-            lastBackpack = if (intArg < 0 || intArg > 18) -1 else intArg
-        }
-
-        return true
-    }
-
-    private fun handleEnderChest(event: MessageSendToServerEvent): Boolean {
-        if (!event.message.startsWith("/enderchest", ignoreCase = true) &&
-            !event.message.startsWith("/ec", ignoreCase = true)
-        ) {
-            return false
-        }
-
-        if (event.message.startsWith("/enderchest -", ignoreCase = true) ||
-            event.message.startsWith("/ec -", ignoreCase = true)
+        if (message.startsWith("${commandPrefix[0]} -") ||
+            message.startsWith("${commandPrefix[1]} -")
         ) {
             event.isCanceled = true
-            if (lastEnderChest == -1) {
-                ChatUtils.chat("There is no previous ender chest to reopen!")
-                return true
-            }
-            openLastEnderChest()
-            lastStorageType = StorageType.ENDER_CHEST
-            return true
+            // Opener function will check whether previous value != null
+            openLastStorage(storageType)
+            return
         }
 
-        val parts = event.message.split(" ")
-        if (parts.size <= 1) {
-            // "/ec" without any argument opens ender chest page #1
-            lastEnderChest = 1
-            return true
+        val parts = message.split(" ")
+        val lastStorageVariable = when (storageType) {
+            StorageType.BACKPACK -> ::lastBackpack
+            StorageType.ENDER_CHEST -> ::lastEnderChest
         }
+
+        if (parts.size <= 1) {
+            // No argument means open the first page of the respective storage
+            lastStorageVariable.set(1)
+            lastStorageType = storageType
+            return
+        }
+
         val intArg = parts[1].toIntOrNull()
         if (intArg != null) {
-            // Surprisingly, "/ec [int <= 0]" would still be a valid command and lead to "/ec 1"!
-            lastEnderChest = if (intArg < 1 || intArg > 9) -1 else intArg
+            lastStorageVariable.set(
+                when (storageType) {
+                    // "/bp 0" still is a valid command (leads to the overview menu)
+                    StorageType.BACKPACK -> if (intArg < 0 || intArg > 18) null else intArg
+                    StorageType.ENDER_CHEST -> if (intArg < 1 || intArg > 9) null else intArg
+                }
+            )
+            lastStorageType = storageType
         }
 
-        return true
+        return
     }
 }
