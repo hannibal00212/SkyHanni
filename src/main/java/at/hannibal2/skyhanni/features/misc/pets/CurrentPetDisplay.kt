ea21a70f8a142f4a3e923f9f90f35d02d4e0848f
David Cole <40234707+DavidArthurCole@users.noreply.github.com>
2024-12-10 01:54:16 -0500

    Merge cleanup

--- src/main/java/at/hannibal2/skyhanni/features/misc/pets/CurrentPetDisplay.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/pets/CurrentPetDisplay.kt
@@ -7,12 +7,6 @@ import at.hannibal2.skyhanni.events.GuiRenderEvent
 import at.hannibal2.skyhanni.features.rift.RiftAPI
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.LorenzUtils
-<<<<<<< HEAD
-=======
-import at.hannibal2.skyhanni.utils.RegexUtils.firstMatcher
-import at.hannibal2.skyhanni.utils.RegexUtils.matchMatcher
-import at.hannibal2.skyhanni.utils.RegexUtils.matches
->>>>>>> upstream/beta
 import at.hannibal2.skyhanni.utils.RenderUtils.renderString
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
@@ -21,67 +15,6 @@ object CurrentPetDisplay {
 
     private val config get() = SkyHanniMod.feature.misc.pets
 
-<<<<<<< HEAD
-=======
-    private val patternGroup = RepoPattern.group("misc.currentpet")
-    private val inventorySelectedPetPattern by patternGroup.pattern(
-        "inventory.selected",
-        "§7§7Selected pet: (?<pet>.*)",
-    )
-    private val chatSpawnPattern by patternGroup.pattern(
-        "chat.spawn",
-        "§aYou summoned your §r(?<pet>.*)§r§a!",
-    )
-    private val chatDespawnPattern by patternGroup.pattern(
-        "chat.despawn",
-        "§aYou despawned your §r.*§r§a!",
-    )
-
-    /**
-     * REGEX-TEST: §cAutopet §eequipped your §7[Lvl 100] §6Griffin§4 ✦§e! §a§lVIEW RULE
-     * REGEX-TEST: §cAutopet §eequipped your §7[Lvl 100] §6Elephant§e! §a§lVIEW RULE
-     */
-    private val chatPetRulePattern by patternGroup.pattern(
-        "chat.rule",
-        "§cAutopet §eequipped your §7\\[Lvl .*] (?<pet>.*)§e! §a§lVIEW RULE",
-    )
-
-    @SubscribeEvent
-    fun onChat(event: LorenzChatEvent) {
-        findPetInChat(event.message)?.let {
-            PetAPI.currentPet = it
-            if (config.hideAutopet) {
-                event.blockedReason = "pets"
-            }
-        }
-    }
-
-    private fun findPetInChat(message: String): String? {
-        chatSpawnPattern.matchMatcher(message) {
-            return group("pet")
-        }
-        if (chatDespawnPattern.matches(message)) {
-            return ""
-        }
-        chatPetRulePattern.matchMatcher(message) {
-            return group("pet")
-        }
-
-        return null
-    }
-
-    @SubscribeEvent
-    fun onInventoryOpen(event: InventoryFullyOpenedEvent) {
-        if (!PetAPI.isPetMenu(event.inventoryName)) return
-
-        val lore = event.inventoryItems[4]?.getLore() ?: return
-        inventorySelectedPetPattern.firstMatcher(lore) {
-            val newPet = group("pet")
-            PetAPI.currentPet = if (newPet != "§cNone") newPet else ""
-        }
-    }
-
->>>>>>> upstream/beta
     @SubscribeEvent
     fun onRenderOverlay(event: GuiRenderEvent.GuiOverlayRenderEvent) {
         if (!LorenzUtils.inSkyBlock) return
