e2e593320f47f707e3e4a009e6aa0f9fdda4b9b3
Not a cow <kly3333aswd@gmail.com>
2024-12-10 19:48:13 -0800

    fixes

--- src/main/java/at/hannibal2/skyhanni/features/misc/PingDisplay.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/PingDisplay.kt
@@ -6,8 +6,7 @@ import at.hannibal2.skyhanni.config.commands.CommandCategory
 import at.hannibal2.skyhanni.config.commands.CommandRegistrationEvent
 import at.hannibal2.skyhanni.config.enums.OutsideSbFeature
 import at.hannibal2.skyhanni.events.GuiRenderEvent
-import at.hannibal2.skyhanni.events.ProfileJoinEvent
-import at.hannibal2.skyhanni.events.minecraft.ClientDisconnectEvent
+import at.hannibal2.skyhanni.events.SecondPassedEvent
 import at.hannibal2.skyhanni.events.minecraft.packet.PacketReceivedEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ChatUtils
@@ -15,11 +14,8 @@ import at.hannibal2.skyhanni.utils.LorenzUtils
 import at.hannibal2.skyhanni.utils.RenderUtils.renderString
 import net.minecraft.client.Minecraft
 import net.minecraft.network.play.client.C16PacketClientStatus
-import net.minecraft.network.play.server.S01PacketJoinGame
 import net.minecraft.network.play.server.S37PacketStatistics
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
 import kotlin.math.abs
 import kotlin.math.round
 
@@ -28,14 +24,11 @@ object PingDisplay {
 
     private val config get() = SkyHanniMod.feature.gui
 
-    private val mc: Minecraft = Minecraft.getMinecraft()
     private var lastPingAt: Long = -1L
     private var invokedCommand = false
-    private var autoPingerEnabled = false
     private var display: String? = null
 
     var latestPing: Double = 0.0
-
 
     fun sendPing(command: Boolean) {
         if (lastPingAt > 0) {
@@ -44,7 +37,7 @@ object PingDisplay {
                 return
             }
         }
-        mc.thePlayer.sendQueue.networkManager.sendPacket(
+        Minecraft.getMinecraft().thePlayer.sendQueue.networkManager.sendPacket(
             C16PacketClientStatus(C16PacketClientStatus.EnumState.REQUEST_STATS)
         )
         lastPingAt = System.nanoTime()
@@ -52,25 +45,11 @@ object PingDisplay {
     }
 
     @HandleEvent
-    fun onProfileJoin(event: ProfileJoinEvent) {
-        if (autoPingerEnabled) return
-        lastPingAt = -1L
-        invokedCommand = false
-        startPingUpdater()
-    }
-
-
-    @HandleEvent
     fun onPacketReceived(event: PacketReceivedEvent) {
+        if (!isEnabled()) return
         val packet = event.packet
         if (lastPingAt > 0) {
             when (packet) {
-                is S01PacketJoinGame -> {
-                    lastPingAt = -1L
-                    invokedCommand = false
-                    startPingUpdater()
-                }
-
                 is S37PacketStatistics -> {
                     val diff = abs(System.nanoTime() - lastPingAt) / 1_000_000.0
                     lastPingAt = -1L
@@ -84,7 +63,6 @@ object PingDisplay {
             }
         }
     }
-
 
     @HandleEvent
     fun onCommandRegister(event: CommandRegistrationEvent) {
@@ -106,20 +84,10 @@ object PingDisplay {
         return "§$color${round(ping * 100) / 100} §7ms"
     }
 
-
-    @HandleEvent
-    fun onDisconnect(event: ClientDisconnectEvent) {
-        autoPingerEnabled = false
-    }
-
-    fun startPingUpdater() {
-        if (autoPingerEnabled) return
-        autoPingerEnabled = true
-        val scheduler = Executors.newScheduledThreadPool(1)
-        ChatUtils.debug("Starting Ping Updater")
-        scheduler.scheduleAtFixedRate({
-            sendPing(false)
-        }, 0, 5, TimeUnit.SECONDS)
+    @SubscribeEvent
+    fun onSecondPassed(event: SecondPassedEvent) {
+        if (!isEnabled()) return
+        sendPing(false)
     }
 
     private fun updateDisplay() {
@@ -132,7 +100,6 @@ object PingDisplay {
 
         config.pingDisplayPosition.renderString(display, posLabel = "Ping Display")
     }
-
 
     private fun isEnabled() = config.pingDisplay && (LorenzUtils.inSkyBlock || OutsideSbFeature.PING_DISPLAY.isSelected())
 }
