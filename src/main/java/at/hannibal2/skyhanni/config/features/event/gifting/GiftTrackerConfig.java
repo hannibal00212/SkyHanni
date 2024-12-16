9a71cca66d1947223fdcac971f0727665aab08e7
David Cole <40234707+DavidArthurCole@users.noreply.github.com>
2024-12-15 21:16:14 -0500

    Should probably add types, yeah

--- src/main/java/at/hannibal2/skyhanni/config/features/event/gifting/GiftTrackerConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/event/gifting/GiftTrackerConfig.java
@@ -2,6 +2,8 @@ package at.hannibal2.skyhanni.config.features.event.gifting;
 
 import at.hannibal2.skyhanni.config.core.config.Position;
 import com.google.gson.annotations.Expose;
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean;
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorInfoText;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigLink;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
 
@@ -9,6 +11,7 @@ public class GiftTrackerConfig {
 
     @Expose
     @ConfigOption(name = "Enabled", desc = "Enable the gift profit tracker.")
+    @ConfigEditorBoolean
     public boolean enabled = false;
 
     @Expose
@@ -17,10 +20,12 @@ public class GiftTrackerConfig {
         desc = "§cDue to the complexities of gifts leaving and re-entering the inventory or stash, gift usage is not auto-tracked. " +
             "§cUse §e/shaddusedgifts §cto manually add gifts used."
     )
+    @ConfigEditorInfoText
     public String note = "";
 
     @Expose
     @ConfigOption(name = "Holding Gift", desc = "Only show the tracker while holding a gift.")
+    @ConfigEditorBoolean
     public boolean holdingGift = false;
 
     @Expose
