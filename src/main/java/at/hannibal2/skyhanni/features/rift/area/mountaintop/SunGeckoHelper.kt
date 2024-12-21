89b4b5e15383d7727aae8ea5598eaeb33008c2f2
nopo <nopotheemail@gmail.com>
2024-12-21 13:48:18 +1100

    mob detekt

--- src/main/java/at/hannibal2/skyhanni/features/rift/area/mountaintop/SunGeckoHelper.kt
+++ src/main/java/at/hannibal2/skyhanni/features/rift/area/mountaintop/SunGeckoHelper.kt
@@ -4,9 +4,12 @@ import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.config.core.config.Position
 import at.hannibal2.skyhanni.config.features.rift.area.mountaintop.SunGeckoConfig
+import at.hannibal2.skyhanni.data.mob.Mob
 import at.hannibal2.skyhanni.events.ActionBarUpdateEvent
 import at.hannibal2.skyhanni.events.GuiRenderEvent
+import at.hannibal2.skyhanni.events.InventoryUpdatedEvent
 import at.hannibal2.skyhanni.events.LorenzChatEvent
+import at.hannibal2.skyhanni.events.LorenzTickEvent
 import at.hannibal2.skyhanni.events.MobEvent
 import at.hannibal2.skyhanni.events.ScoreboardUpdateEvent
 import at.hannibal2.skyhanni.events.skyblock.ScoreboardAreaChangeEvent
@@ -14,18 +17,16 @@ import at.hannibal2.skyhanni.features.rift.RiftAPI
 import at.hannibal2.skyhanni.mixins.hooks.RenderLivingEntityHelper
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ColorUtils.addAlpha
-import at.hannibal2.skyhanni.utils.EntityUtils
-import at.hannibal2.skyhanni.utils.LocationUtils.distanceToPlayer
 import at.hannibal2.skyhanni.utils.RegexUtils.findMatcher
 import at.hannibal2.skyhanni.utils.RegexUtils.groupOrNull
-import at.hannibal2.skyhanni.utils.RegexUtils.matchMatcher
 import at.hannibal2.skyhanni.utils.RenderUtils.renderStrings
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import at.hannibal2.skyhanni.utils.StringUtils.allLettersFirstUppercase
 import at.hannibal2.skyhanni.utils.TimeUtils
 import at.hannibal2.skyhanni.utils.TimeUtils.format
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
-import net.minecraft.entity.Entity
+import net.minecraft.init.Blocks
+import net.minecraft.item.Item
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import java.awt.Color
 import kotlin.time.Duration
@@ -39,15 +40,6 @@ object SunGeckoHelper {
     var display: ArrayList<String> = ArrayList()
     var modifiers: MutableSet<MODIFIERS> = mutableSetOf()
     private val patternGroup = RepoPattern.group("rift.area.mountaintop.sungecko")
-
-    /**
-     * REGEX-TEST: §a﴾ §8[§7Lv20§8] §c§l§eSun Gecko§r§r §e45§f/§a250§c❤ §a﴿
-     * REGEX-TEST: §a﴾ §8[§7Lv20§8] §c§l§eSun Gecko§r§r §a250§f/§a250§c❤ §a﴿
-     */
-    private val sunGeckoName by patternGroup.pattern(
-        "name",
-        "§a﴾ §8\\[§7Lv20§8] §c§l§eSun Gecko§r§r (?<healthColor>§[aec])(?<healthLeft>\\d+)§f/§a(?<totalHealth>\\d+)§c❤ §a﴿"
-    )
 
     /**
      * REGEX-TEST: §a[⬛⬛⬛⬜§c⬜ §e§lx2 §c⬜⬜⬜⬜⬜]
@@ -65,25 +57,25 @@ object SunGeckoHelper {
     private var healthLeft = 250
     private var totalHealth = 250
     private var actionBarFormatted: String = ""
-    private var healthColor: String = "§a"
     private var onFirstPhase = true
     private var timeSliceDuration = Duration.ZERO
     private var timeSinceLastHit = SimpleTimeMark.farPast()
     private var combo = 1
     private var scanningChat = false
     private var inTimeChamber = false
+    private var currentBoss: Mob? = null
 
     fun reset() {
+        display.clear()
         healthLeft = 250
         totalHealth = 250
         actionBarFormatted = ""
-        healthColor = "§a"
         onFirstPhase = true
         timeSliceDuration = Duration.ZERO
         timeSinceLastHit = SimpleTimeMark.farPast()
         combo = 1
-        scanningChat = false
         inTimeChamber = false
+        currentBoss = null
     }
 
     fun updateDisplay() {
@@ -93,6 +85,11 @@ object SunGeckoHelper {
         if (modifiers.contains(MODIFIERS.REVIVAL)) {
             if (onFirstPhase) displayHealthLeft += totalHealth
             displayTotalHealth *= 2
+        }
+
+        var healthColor = "§a"
+        if (displayHealthLeft <= displayTotalHealth / 2) {
+            healthColor = "§e"
         }
 
         val health = "$healthColor$displayHealthLeft§f/§a$displayTotalHealth§c❤"
@@ -129,13 +126,40 @@ object SunGeckoHelper {
                 event.mob.baseEntity,
                 Color.RED.addAlpha(80),
             ) { config.highlightFakeBoss }
-        } else if (config.highlightRealBoss) {
-            RenderLivingEntityHelper.setEntityColorWithNoHurtTime(
-                event.mob.baseEntity,
-                Color.GREEN.addAlpha(80),
-            ) { config.highlightRealBoss }
+        } else {
+            if (config.highlightRealBoss) {
+                RenderLivingEntityHelper.setEntityColorWithNoHurtTime(
+                    event.mob.baseEntity,
+                    Color.GREEN.addAlpha(80),
+                ) { config.highlightRealBoss }
+            }
+            currentBoss = event.mob
         }
 
+    }
+
+    @SubscribeEvent
+    fun onTick(event: LorenzTickEvent) {
+        if (!isEnabled()) {
+            return
+        }
+        if (!inTimeChamber) return
+
+        if (currentBoss?.baseEntity?.isEntityAlive == false) {
+            currentBoss = null
+        }
+
+        if (currentBoss == null) return
+
+        val health = currentBoss?.health?.toInt() ?: -1
+        if (health > healthLeft) {
+            onFirstPhase = false
+            modifiers.add(MODIFIERS.REVIVAL)
+        }
+        healthLeft = health
+        totalHealth = currentBoss?.maxHealth ?: 250
+
+        updateDisplay()
     }
 
 
@@ -144,33 +168,23 @@ object SunGeckoHelper {
         if (!isEnabled()) return
         if (!inTimeChamber) return
 
-        var nearestEntityDistance = 100.0
-        var nearestEntity: Entity? = null
-        for (entity in EntityUtils.getAllEntities()) {
-            sunGeckoName.matchMatcher(entity.name) {
-                if (entity.distanceToPlayer() < nearestEntityDistance) {
-                    nearestEntityDistance = entity.distanceToPlayer()
-                    nearestEntity = entity
-                }
-            }
-        }
-
-        if (nearestEntity == null) return
-
-        sunGeckoName.matchMatcher(nearestEntity?.name.orEmpty()) {
-            val health = group("healthLeft")?.toIntOrNull() ?: -1
-            if (health > healthLeft) {
-                onFirstPhase = false
-                modifiers.add(MODIFIERS.REVIVAL)
-            }
-            healthLeft = health
-            totalHealth = group("totalHealth")?.toIntOrNull() ?: 250
-            healthColor = group("healthColor")?.toString() ?: "§a"
-        }
-
-        updateDisplay()
-
         pos.renderStrings(display, 0, "Sun Gecko Helper")
+    }
+
+    @SubscribeEvent
+    fun onInventoryUpdated(event: InventoryUpdatedEvent) {
+        if (!isEnabled()) return
+        if (event.inventoryName != "Modifiers") return
+        modifiers.clear()
+
+        for (modifier in MODIFIERS.entries) {
+            val slot = modifier.slot
+            val item = event.inventoryItems[slot] ?: continue
+            if (item.item != Item.getItemFromBlock(Blocks.stained_glass_pane)) continue
+            if (item.itemDamage != 5) continue
+            modifiers.add(modifier)
+        }
+
     }
 
     @HandleEvent
@@ -228,6 +242,7 @@ object SunGeckoHelper {
         if (!isEnabled()) return
         if (event.message == "§f                           §r§c§lACTIVE MODIFIERS!") {
             scanningChat = true
+            modifiers.clear()
         } else if (event.message == "§6§l▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬") {
             scanningChat = false
         }
@@ -252,15 +267,14 @@ object SunGeckoHelper {
 
     fun isEnabled() = config.enabled && RiftAPI.inRift() && RiftAPI.inMountainTop()
 
-    enum class MODIFIERS {
-        REVIVAL, // spawns a second dude
-        COMBO_MANIC,
-        TIME_SLICED, // reduces combo lvl up by 1 for 30 seconds only
-        BUFFANTICS,
-        COLLECTIVE, // increase time for combo by 0.2 per modifier
-        BRAND_NEW_DANCE,
-        CULMINATION, // reduces combo lvl up by 1
+    enum class MODIFIERS(val slot: Int) {
+        REVIVAL(19), // spawns a second dude
+        COMBO_MANIC(20),
+        TIME_SLICED(21), // reduces combo lvl up by 1 for 30 seconds only
+        BUFFANTICS(22),
+        COLLECTIVE(23), // increase time for combo by 0.2 per modifier
+        BRAND_NEW_DANCE(24),
+        CULMINATION(25), // reduces combo lvl up by 1
     }
-
 
 }
