8e5b16ee4059b5424e775d017faba1176613b643
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2025-01-04 18:57:10 +0100

    using proper json names

--- src/main/java/at/hannibal2/skyhanni/data/jsonobjects/repo/EnchantedClockJson.kt
+++ src/main/java/at/hannibal2/skyhanni/data/jsonobjects/repo/EnchantedClockJson.kt
@@ -1,17 +1,18 @@
 package at.hannibal2.skyhanni.data.jsonobjects.repo
 
 import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
 
 data class EnchantedClockJson(
-    @Expose val boosts: List<BoostJson>
+    @Expose val boosts: List<BoostJson>,
 )
 
 data class BoostJson(
     @Expose val name: String,
-    @Expose val displayName: String,
-    @Expose val usageString: String?,
+    @Expose @SerializedName("display_name") val displayName: String,
+    @Expose @SerializedName("usage_string") val usageString: String?,
     @Expose val color: String,
-    @Expose val displaySlot: Int,
-    @Expose val statusSlot: Int,
-    @Expose val cooldownHours: Int = 48,
+    @Expose @SerializedName("display_slot") val displaySlot: Int,
+    @Expose @SerializedName("status_slot") val statusSlot: Int,
+    @Expose @SerializedName("usage_hours") val cooldownHours: Int = 48,
 )
