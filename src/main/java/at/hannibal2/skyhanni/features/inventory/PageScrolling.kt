f6dc96260896746daeb47f136f74d2d7b5d8ee10
Cédric <142994683+Cedric-Ab@users.noreply.github.com>
2025-01-06 19:29:08 +0100

    remove unnecessary cooldown

--- src/main/java/at/hannibal2/skyhanni/features/inventory/PageScrolling.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/PageScrolling.kt
@@ -8,13 +8,10 @@ import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.KeyboardManager.isKeyHeld
 import at.hannibal2.skyhanni.utils.LorenzUtils
 import at.hannibal2.skyhanni.utils.RegexUtils.matches
-import at.hannibal2.skyhanni.utils.SimpleTimeMark
-import at.hannibal2.skyhanni.utils.SimpleTimeMark.Companion.fromNow
 import at.hannibal2.skyhanni.utils.renderables.ScrollValue
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import org.lwjgl.input.Mouse
-import kotlin.time.Duration.Companion.seconds
 
 @SkyHanniModule
 object PageScrolling {
@@ -49,13 +46,10 @@ object PageScrolling {
 
     private val scroll = ScrollValue()
 
-    private var cooldown = SimpleTimeMark.farPast()
-
     @SubscribeEvent
     fun onLorenzTick(event: LorenzTickEvent) {
         if (!isEnabled()) return
         if (InventoryUtils.inStorage() && InventoryUtils.isNeuStorageEnabled) return
-        if (cooldown.isInFuture()) return
         if (!scroll.isMouseEventValid()) return
 
         val inventoryName = InventoryUtils.openInventoryName()
@@ -73,7 +67,6 @@ object PageScrolling {
             patterns.matches(it.stack?.displayName)
         } ?: return
         InventoryUtils.clickSlot(slot.slotNumber)
-        cooldown = 1.0.seconds.fromNow() // 1 second is not specific it is just a reasonable cooldown
     }
 
     fun isEnabled() = LorenzUtils.inSkyBlock && config.enable && InventoryUtils.inInventory()
