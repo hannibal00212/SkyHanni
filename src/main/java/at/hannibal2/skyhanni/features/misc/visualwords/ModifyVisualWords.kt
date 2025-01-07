f3b6b43a2bf797a592ac95f5d344704f51a54738
David Cole <40234707+DavidArthurCole@users.noreply.github.com>
2025-01-06 22:15:31 -0500

    Slight improvements

--- src/main/java/at/hannibal2/skyhanni/features/misc/visualwords/ModifyVisualWords.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/visualwords/ModifyVisualWords.kt
@@ -23,7 +23,7 @@ object ModifyVisualWords {
     var userModifiedWords = mutableListOf<VisualWord>()
 
     // Replacements the mod added automatically for some features, april jokes, etc
-    private var modModifiedWords = mutableListOf<VisualWord>()
+    private val modModifiedWords = mutableListOf<VisualWord>()
     private var finalWordsList = listOf<VisualWord>()
     private var debug = false
 
@@ -86,6 +86,7 @@ object ModifyVisualWords {
     }
 
     @HandleEvent
+    @Suppress("DEPRECATION")
     fun onHypixelJoin(event: HypixelJoinEvent) {
         val oldModifiedWords = SkyHanniMod.feature.storage.modifiedWords
         if (oldModifiedWords.isNotEmpty()) {
