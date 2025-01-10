f5c784c7614b77f4edbddd2237a5cffb31c6b87b
Chiss5618 <78828070+Chiss5618@users.noreply.github.com>
2025-01-09 19:55:14 -0600

    make nullable

--- src/main/java/at/hannibal2/skyhanni/features/garden/GardenAPI.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/GardenAPI.kt
@@ -169,7 +169,7 @@ object GardenAPI {
         return CropType.entries.firstOrNull { internalName.startsWith(it.toolName) }
     }
 
-    fun readCounter(itemStack: ItemStack): Long = itemStack.getHoeCounter() ?: itemStack.getCultivatingCounter() ?: -1L
+    fun readCounter(itemStack: ItemStack): Long? = itemStack.getHoeCounter() ?: itemStack.getCultivatingCounter()
 
     fun MutableList<Renderable>.addCropIcon(
         crop: CropType,
