70cc706e2df76dbaa6b419325f20bbecc7395336
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-01-10 16:01:30 +0100

    there (please lets talk about this change)
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/events/LorenzTickEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/LorenzTickEvent.kt
@@ -4,6 +4,9 @@ class LorenzTickEvent(private val tick: Int) : LorenzEvent() {
 
     fun isMod(i: Int, offset: Int = 0) = (tick + offset) % i == 0
 
-    @Deprecated("Use SecondPassedEvent instead", ReplaceWith(""))
+    /**
+     * Use of this method is discouraged, use [SecondPassedEvent] instead.
+     * Only use if very needed.
+     */
     fun repeatSeconds(i: Int, offset: Int = 0) = isMod(i * 20, offset)
 }
