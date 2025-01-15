8607dd3905f3bb56c095a3c1215fa084f14371e1
David Cole <40234707+DavidArthurCole@users.noreply.github.com>
2025-01-15 00:28:28 -0500

    Add tests

--- src/main/java/at/hannibal2/skyhanni/features/chat/ShortenCoins.kt
+++ src/main/java/at/hannibal2/skyhanni/features/chat/ShortenCoins.kt
@@ -15,12 +15,21 @@ object ShortenCoins {
     private val patternGroup = RepoPattern.group("chat.coins")
 
     /**
-     * REGEX-TEST: §6[Auction] §aEuropaPlus §ebought §fAtmospheric Filter §efor §62,650,000 coins §lCLICK
-     * REGEX-TEST: §aYou sold §r§aCicada Symphony Vinyl§r§8 x1 §r§afor §r§650,000 Coins§r§a!
+     * Because this regex isn't matching the whole message but parts of it, the real regex
+     * tests can't be included here. The regex tests are just the coins section of the message.
+     *
+     * §6[Auction] §aEuropaPlus §ebought §fAtmospheric Filter §efor §62,650,000 coins §lCLICK
+     * §aYou sold §r§aCicada Symphony Vinyl§r§8 x1 §r§afor §r§650,000 Coins§r§a!
+     * §6§lALLOWANCE! §r§eYou earned §r§650,000 coins§r§e!
+     * §6[Bazaar] §r§7§r§eSell Offer Setup! §r§a5§r§7x §r§9Enchanted Melon Block §r§7for §r§6250,303 coins§r§7.
+     *
+     * REGEX-TEST: §62,650,000
+     * REGEX-TEST: §r§650,000
+     * REGEX-TEST: §r§6250,303
      */
     private val coinsPattern by patternGroup.pattern(
         "format",
-        "§6(?<amount>[\\d,]+)"
+        "(?:§.)*§6(?<amount>[\\d,]+)"
     )
 
     @SubscribeEvent
