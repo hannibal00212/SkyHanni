bee19aba514b85e16342ed51211da3f3af17b35e
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2025-01-18 21:59:30 +0100

    formatting

--- src/main/java/at/hannibal2/skyhanni/config/features/garden/pests/PestWarningSoundSettings.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/garden/pests/PestWarningSoundSettings.java
@@ -17,7 +17,11 @@ public class PestWarningSoundSettings {
     public String name = "random.orb";
 
     @Expose
-    @ConfigOption(name = "Repeat Sound Notification", desc = "Repeat the sound notification until wardrobe is opened or pests spawn" + "§eRecommended §lonly §rif you're pest farming!")
+    @ConfigOption(
+        name = "Repeat Sound Notification",
+        desc = "Repeat the sound notification until wardrobe is opened or pests spawn" +
+            "§eRecommended §lonly §rif you're pest farming!"
+    )
     @ConfigEditorBoolean
     public boolean repeatSound = false;
 
