f7f6ed9650ca8bb7ecb5746a7cb43d468a2db3d9
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-01-19 16:13:09 +0100

    fix merge
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/elements/ScoreboardElementSkyBlockXp.kt
+++ src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/elements/ScoreboardElementSkyBlockXp.kt
@@ -1,18 +1,14 @@
 package at.hannibal2.skyhanni.features.gui.customscoreboard.elements
 
 import at.hannibal2.skyhanni.api.SkyBlockXPAPI
-import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboard
+import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboardUtils
 
 object ScoreboardElementSkyBlockXp : ScoreboardElement() {
     override fun getDisplay() = buildList {
         val (level, xp) = SkyBlockXPAPI.levelXpPair ?: return@buildList
-        if (CustomScoreboard.displayConfig.displayNumbersFirst) {
-            add("${SkyBlockXPAPI.getLevelColor().getChatColor()}$level SB Level")
-            add("§b$xp§3/§b100 XP")
-        } else {
-            add("SB Level: ${SkyBlockXPAPI.getLevelColor().getChatColor()}$level")
-            add("XP: §b$xp§3/§b100")
-        }
+        val color = SkyBlockXPAPI.getLevelColor().getChatColor()
+        add(CustomScoreboardUtils.formatScoreboardNumberDisplayDisplay("SB Level", level.toString(), color))
+        add(CustomScoreboardUtils.formatScoreboardNumberDisplayDisplay("XP", "$xp§3/§b100", "§b"))
     }
 
     override fun showWhen() = SkyBlockXPAPI.levelXpPair != null
