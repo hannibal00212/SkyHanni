75253059f6577c56f51a257e07a00b0d4d9c490e
Cal <cwolfson58@gmail.com>
2025-01-24 12:05:07 +1100

    oops

--- src/main/java/at/hannibal2/skyhanni/api/enoughupdates/ItemResolutionQuery.kt
+++ src/main/java/at/hannibal2/skyhanni/api/enoughupdates/ItemResolutionQuery.kt
@@ -122,18 +122,12 @@ class ItemResolutionQuery {
                 val name = group("name").trim().replace("'", "")
                 val ultimate = group("format").lowercase().contains("§l")
                 val prefix = if (ultimate && name != "Ultimate Wise" && name != "Ultimate Jerry") "ULTIMATE_" else ""
-                val cleanedEnchantName = name.turboCheck().renamedEnchantmentCheck().replace(" ", "_").replace("-", "_").uppercase()
+                val cleanedEnchantName = name.renamedEnchantmentCheck().replace(" ", "_").replace("-", "_").uppercase()
                 "$prefix$cleanedEnchantName;${group("level").romanToDecimal()}".uppercase()
             }
-
-        private fun String.turboCheck(): String {
+        private fun String.renamedEnchantmentCheck(): String {
             if (this == "Turbo-Cocoa") return "Turbo-Coco"
             if (this == "Turbo-Cacti") return "Turbo-Cactus"
-
-            return this
-        }
-
-        private fun String.renamedEnchantmentCheck(): String {
             if (this == "Prismatic") return "Pristine"
             if (this == "Dragon Tracer") return "Aiming"
 
