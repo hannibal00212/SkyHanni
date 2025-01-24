943b7fff97d5bf868f2ec7776b131b475edaf897
Cal <cwolfson58@gmail.com>
2025-01-24 12:53:07 +1100

    fix merge

--- src/main/java/at/hannibal2/skyhanni/features/rift/area/colosseum/BactePhase.kt
+++ src/main/java/at/hannibal2/skyhanni/features/rift/area/colosseum/BactePhase.kt
@@ -4,18 +4,16 @@ import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.data.IslandType
 import at.hannibal2.skyhanni.data.mob.Mob
-import at.hannibal2.skyhanni.events.LorenzChatEvent
 import at.hannibal2.skyhanni.events.MobEvent
 import at.hannibal2.skyhanni.events.SecondPassedEvent
+import at.hannibal2.skyhanni.events.chat.SkyHanniChatEvent
 import at.hannibal2.skyhanni.events.minecraft.RenderWorldEvent
 import at.hannibal2.skyhanni.features.rift.RiftAPI
-import at.hannibal2.skyhanni.features.rift.area.colosseum.BactePhase.BactePhase.entries
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.RegexUtils.matchMatcher
 import at.hannibal2.skyhanni.utils.RenderUtils.drawDynamicText
 import at.hannibal2.skyhanni.utils.getLorenzVec
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 @SkyHanniModule
 object BactePhase {
@@ -56,8 +54,8 @@ object BactePhase {
     var currentPhase = BactePhase.NOT_ACTIVE
     private var bacte: Mob? = null
 
-    @SubscribeEvent
-    fun onChatMessage(event: LorenzChatEvent) {
+    @HandleEvent(onlyOnIsland = IslandType.THE_RIFT)
+    fun onChatMessage(event: SkyHanniChatEvent) {
         nameChatPattern.matchMatcher(event.message) {
             currentPhase = BactePhase.fromNumber(group("name").length)
         }
