bf40b0566ae0e73ff82df19d8feec6a7b7f107d4
Linnea Gräf <nea@nea.moe>
2025-01-24 14:20:34 +0100

    Backend: Warn for immutable types in the config
    Depends on: https://github.com/hannibal002/SkyHanni/pull/3204

--- detekt/build.gradle.kts
+++ detekt/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm")
     id("com.google.devtools.ksp")
+    id("io.gitlab.arturbosch.detekt")
 }
 
 dependencies {
@@ -10,4 +11,5 @@ dependencies {
     implementation("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.7")
     testImplementation("io.kotest:kotest-assertions-core:5.9.1")
     testImplementation("io.gitlab.arturbosch.detekt:detekt-test:1.23.7")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-rules-ruleauthors:1.23.7")
 }
