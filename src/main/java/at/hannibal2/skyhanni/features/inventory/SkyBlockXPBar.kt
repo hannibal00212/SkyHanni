bbbadefa429149eca5ab78846c46dbf3037bf7d4
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-01-24 17:09:31 +0100

    fixed skyblock xp bar overriding other mods
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/inventory/SkyBlockXPBar.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/SkyBlockXPBar.kt
@@ -13,13 +13,29 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 @SkyHanniModule
 object SkyBlockXPBar {
     private val config get() = SkyHanniMod.feature.misc
+    private var playerExperience: Float = 0f
+    private var playerExperienceTotal: Int = 0
+    private var playerExperienceLevel: Int = 0
 
     @SubscribeEvent
     fun onRenderExperienceBar(event: RenderGameOverlayEvent.Pre) {
         if (!isEnabled()) return
         if (event.type != RenderGameOverlayEvent.ElementType.EXPERIENCE) return
+        val thePlayer = Minecraft.getMinecraft().thePlayer
+        playerExperience = thePlayer.experience
+        playerExperienceTotal = thePlayer.experienceTotal
+        playerExperienceLevel = thePlayer.experienceLevel
         val (level, xp) = SkyBlockXPAPI.levelXpPair ?: return
-        Minecraft.getMinecraft().thePlayer.setXPStats(xp / 100f, 100, level)
+
+        thePlayer.setXPStats(xp / 100f, 100, level)
+    }
+
+    @SubscribeEvent
+    fun onRenderExperienceBarPost(event: RenderGameOverlayEvent.Post) {
+        if (!isEnabled()) return
+        if (event.type != RenderGameOverlayEvent.ElementType.EXPERIENCE) return
+        val thePlayer = Minecraft.getMinecraft().thePlayer
+        thePlayer.setXPStats(playerExperience, playerExperienceTotal, playerExperienceLevel)
     }
 
     private fun isEnabled() = LorenzUtils.inSkyBlock && !inAnyIsland(IslandType.THE_RIFT, IslandType.CATACOMBS) && config.skyblockXpBar
