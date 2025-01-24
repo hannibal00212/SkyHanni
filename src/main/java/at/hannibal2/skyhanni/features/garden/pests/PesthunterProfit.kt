450225a1c2eb318462dbdeb68c053768275d3aee
David Cole <40234707+DavidArthurCole@users.noreply.github.com>
2025-01-24 02:40:10 -0500

    Init

--- src/main/java/at/hannibal2/skyhanni/features/garden/pests/PesthunterProfit.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/pests/PesthunterProfit.kt
@@ -4,36 +4,40 @@ import at.hannibal2.skyhanni.events.GuiRenderEvent
 import at.hannibal2.skyhanni.events.InventoryCloseEvent
 import at.hannibal2.skyhanni.events.InventoryFullyOpenedEvent
 import at.hannibal2.skyhanni.features.garden.GardenAPI
-import at.hannibal2.skyhanni.features.inventory.patternGroup
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.test.command.ErrorManager
 import at.hannibal2.skyhanni.utils.DisplayTableEntry
-import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.ItemPriceUtils.getPrice
 import at.hannibal2.skyhanni.utils.ItemUtils
 import at.hannibal2.skyhanni.utils.ItemUtils.getInternalName
 import at.hannibal2.skyhanni.utils.ItemUtils.getLore
-import at.hannibal2.skyhanni.utils.ItemUtils.itemName
 import at.hannibal2.skyhanni.utils.LorenzUtils
 import at.hannibal2.skyhanni.utils.NEUInternalName
 import at.hannibal2.skyhanni.utils.NumberUtil.formatDoubleOrNull
 import at.hannibal2.skyhanni.utils.NumberUtil.shortFormat
-import at.hannibal2.skyhanni.utils.RegexUtils.matchMatcher
+import at.hannibal2.skyhanni.utils.RegexUtils.firstMatcher
+import at.hannibal2.skyhanni.utils.RegexUtils.matches
 import at.hannibal2.skyhanni.utils.RenderUtils.renderRenderables
 import at.hannibal2.skyhanni.utils.renderables.Renderable
+import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 import net.minecraft.item.ItemStack
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 @SkyHanniModule
 object PesthunterProfit {
 
-    data class PesthunterTrade(val internalName: NEUInternalName, val coinsPerPest: Double)
-    var bestPesthunterTrade = mutableListOf<PesthunterTrade>()
-
     private val config get() = GardenAPI.config.pests.pesthunterShop
+    private val patternGroup = RepoPattern.group("garden.pests.pesthunter")
+    private val DENY_LIST_ITEMS = listOf(
+        "§cClose",
+        "§6Pesthunter's Wares",
+        " ",
+    )
     private var display = emptyList<Renderable>()
-
+    private var bestPesthunterTrade = mutableListOf<PesthunterTrade>()
     var inInventory = false
+
+    data class PesthunterTrade(val internalName: NEUInternalName, val coinsPerPest: Double)
 
     /**
      * REGEX-TEST: §2100 Pests
@@ -55,43 +59,26 @@ object PesthunterProfit {
         if (event.inventoryName != "Pesthunter's Wares") return
 
         inInventory = true
-
-        val table = mutableListOf<DisplayTableEntry>()
-        for ((slot, item) in event.inventoryItems) {
-            try {
-                readItem(slot, item, table)
-            } catch (e: Throwable) {
-                ErrorManager.logErrorWithData(
-                    e, "Error in PesthunterProfit while reading item '${item.itemName}'",
-                    "name" to item.itemName,
-                    "inventory name" to InventoryUtils.openInventoryName(),
-                )
-            }
-        }
-
-        val newList = mutableListOf<Renderable>()
-        newList.add(Renderable.string("§ePesthunter Shop Profit"))
-        newList.add(LorenzUtils.fillTable(table, padding = 5, itemScale = 0.7))
-        display = newList
+        display = buildRenderables(event.inventoryItems)
     }
 
-    private fun readItem(slot: Int, item: ItemStack, table: MutableList<DisplayTableEntry>) {
-        val itemName = item.displayName
-        if (itemName == " " || itemName == "§cClose") return
-        if (slot == 49 || itemName == "§6Pesthunter's Wares") return
+    private fun buildRenderables(items: Map<Int, ItemStack>) = buildList {
+        val table = items.mapNotNull { (slot, stack) -> readItem(slot, stack) }
+        add(Renderable.string("§ePesthunter Shop Profit"))
+        add(LorenzUtils.fillTable(table, padding = 5, itemScale = 0.7))
+    }
 
-        val totalCost = getFullCost(getRequiredItems(item))
-        if (totalCost < 0) return
+    private fun readItem(slot: Int, item: ItemStack): DisplayTableEntry? {
+        val itemName = item.displayName.takeIf { it !in DENY_LIST_ITEMS } ?: return null
+        if (slot == 49) return null
 
-        val (name, amount) = ItemUtils.readItemAmount(itemName) ?: return
+        val totalCost = getFullCost(getRequiredItems(item)).takeIf { it > 0 } ?: return null
+        val (name, amount) = ItemUtils.readItemAmount(itemName) ?: return null
+        val fixedDisplayName = name.replace("[Lvl 100]", "[Lvl {LVL}]")
+        val internalName = NEUInternalName.fromItemNameOrNull(fixedDisplayName)
+            ?: item.getInternalName()
 
-        var internalName = NEUInternalName.fromItemNameOrNull(name.replace("[Lvl 100]", "[Lvl {LVL}]"))
-        if (internalName == null) {
-            internalName = item.getInternalName()
-        }
-
-        val itemPrice = internalName.getPrice() * amount
-        if (itemPrice < 0) return
+        val itemPrice = (internalName.getPrice() * amount).takeIf { it > 0 } ?: return null
 
         val profit = itemPrice - totalCost
         val pestsCost = getPestsCost(item)
@@ -107,49 +94,37 @@ object PesthunterProfit {
             "§7Profit per pest: §6${profitPerPest.shortFormat()} ",
         )
 
-        table.add(
-            DisplayTableEntry(
-                itemName.replace("[Lvl 100]", "[Lvl 1]"), // show level 1 hedgehog instead of level 100
-                "$color${profitPerPest.shortFormat()}",
-                profitPerPest,
-                internalName,
-                hover,
-                highlightsOnHoverSlots = listOf(slot),
-            ),
-        )
-
-        if (bestPesthunterTrade.isEmpty() || profitPerPest > (
-                bestPesthunterTrade.maxByOrNull {
-                    it.coinsPerPest
-                }?.coinsPerPest ?: 0.0
-                )
-        ) {
+        val bestTrade = bestPesthunterTrade.maxOfOrNull { it.coinsPerPest } ?: 0.0
+        if (bestPesthunterTrade.isEmpty() || profitPerPest > bestTrade) {
             bestPesthunterTrade.clear()
             bestPesthunterTrade.add(PesthunterTrade(internalName, profitPerPest))
         }
+
+        return DisplayTableEntry(
+            itemName.replace("[Lvl 100]", "[Lvl 1]"), // show level 1 hedgehog instead of level 100
+            "$color${profitPerPest.shortFormat()}",
+            profitPerPest,
+            internalName,
+            hover,
+            highlightsOnHoverSlots = listOf(slot),
+        )
     }
 
     private fun getRequiredItems(item: ItemStack): MutableList<String> {
-        val items = mutableListOf<String>()
-        var next = false
+        val lore = item.getLore()
+        // Find the subsection of lore between "§7Cost" and the next empty line
+        val startIndex = lore.indexOf("§7Cost") + 1
+        val endIndex = lore.indexOfFirst { it.isBlank() && lore.indexOf(it) > startIndex }
 
-        for (line in item.getLore()) {
-            when {
-                line == "§7Cost" -> {
-                    next = true
-                }
-                next -> {
-                    if (line.isBlank()) {
-                        next = false
-                    } else {
-                        pestCostPattern.matchMatcher(line) { break }
-                        items.add(line.replace("§8 ", " §8"))
-                    }
-                }
-            }
-        }
+        val costLore =
+            if (endIndex != -1)  lore.subList(startIndex, endIndex)
+            else lore.subList(startIndex, lore.size)
 
-        return items
+        return costLore.filter {
+            !pestCostPattern.matches(it)
+        }.map {
+            it.replace("§8 ", " §8")
+        }.toMutableList()
     }
 
     private fun getFullCost(requiredItems: List<String>): Double {
@@ -170,24 +145,17 @@ object PesthunterProfit {
         return otherItemsPrice
     }
 
-    private fun getPestsCost(item: ItemStack): Int {
-        val lore = item.getLore()
-        for (line in lore) {
-            pestCostPattern.matchMatcher(line) {
-                return group("pests")?.formatDoubleOrNull()?.toInt() ?: 0
-            }
-        }
-        return 0
-    }
+    private fun getPestsCost(item: ItemStack): Int = pestCostPattern.firstMatcher(item.getLore()) {
+        group("pests")?.formatDoubleOrNull()?.toInt() ?: 0
+    } ?: 0
 
     @SubscribeEvent
     fun onBackgroundDraw(event: GuiRenderEvent.ChestGuiOverlayRenderEvent) {
-        if (inInventory) {
-            config.pesthunterProfitPos.renderRenderables(
-                display,
-                extraSpace = 5,
-                posLabel = "Pesthunter Profit",
-            )
-        }
+        if (!inInventory) return
+        config.pesthunterProfitPos.renderRenderables(
+            display,
+            extraSpace = 5,
+            posLabel = "Pesthunter Profit",
+        )
     }
 }
