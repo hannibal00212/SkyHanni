cd6a588e9eae3c765e1af919dc5fabff58bdfd28
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2025-01-26 16:04:33 +0100

    more checks

--- src/main/java/at/hannibal2/skyhanni/features/rift/area/livingcave/LivingCaveSnakeFeatures.kt
+++ src/main/java/at/hannibal2/skyhanni/features/rift/area/livingcave/LivingCaveSnakeFeatures.kt
@@ -1,6 +1,7 @@
 package at.hannibal2.skyhanni.features.rift.area.livingcave
 
 import at.hannibal2.skyhanni.api.event.HandleEvent
+import at.hannibal2.skyhanni.data.ClickType
 import at.hannibal2.skyhanni.events.BlockClickEvent
 import at.hannibal2.skyhanni.events.ServerBlockChangeEvent
 import at.hannibal2.skyhanni.events.minecraft.SkyHanniRenderWorldEvent
@@ -11,8 +12,10 @@ import at.hannibal2.skyhanni.test.graph.GraphEditor.distanceSqToPlayer
 import at.hannibal2.skyhanni.utils.BlockUtils.getBlockAt
 import at.hannibal2.skyhanni.utils.CollectionUtils.editCopy
 import at.hannibal2.skyhanni.utils.CollectionUtils.sorted
+import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.LorenzColor
 import at.hannibal2.skyhanni.utils.LorenzVec
+import at.hannibal2.skyhanni.utils.NeuInternalName.Companion.toInternalName
 import at.hannibal2.skyhanni.utils.RenderUtils.draw3DLine
 import at.hannibal2.skyhanni.utils.RenderUtils.drawString
 import at.hannibal2.skyhanni.utils.RenderUtils.drawWaypointFilled
@@ -20,8 +23,8 @@ import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import net.minecraft.block.Block
 import net.minecraft.client.Minecraft
 import net.minecraft.init.Blocks
-import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
 @SkyHanniModule
 object LivingCaveSnakeFeatures {
@@ -30,21 +33,42 @@ object LivingCaveSnakeFeatures {
 
     class Snake(
         var blocks: List<LorenzVec>,
-        var lastRemoveTime: SimpleTimeMark = SimpleTimeMark.now(),
-        var lastAddTime: SimpleTimeMark = SimpleTimeMark.now(),
-        var phase: Phase = Phase.SPAWNING,
-    )
+        var lastRemoveTime: SimpleTimeMark = SimpleTimeMark.farPast(),
+        var lastAddTime: SimpleTimeMark = SimpleTimeMark.farPast(),
+        var state: State = State.SPAWNING,
+        var lastCalmTime: SimpleTimeMark = SimpleTimeMark.farPast(),
+        var lastBreakTime: SimpleTimeMark = SimpleTimeMark.farPast(),
+    ) {
+        fun invalidSize(): Boolean = blocks.zipWithNext().any { (a, b) ->
+            a.distance(b) > 3
+        }
 
-    enum class Phase(val color: LorenzColor) {
+        fun isNotTouchingAir(): Boolean = blocks.any { it.isNotTouchingAir() }
+
+        fun getInteraction(): Interaction {
+            if (this == selectedSnake) {
+                if (lastBreakTime.passedSince() < 2.seconds) {
+                    return Interaction.BREAKING
+                }
+                if (lastCalmTime.passedSince() < 2.seconds) {
+                    return Interaction.CALMING
+                }
+            }
+
+            return Interaction.NONE
+        }
+    }
+
+    enum class State(val color: LorenzColor) {
         SPAWNING(LorenzColor.AQUA),
-        MOVING(LorenzColor.YELLOW),
+        ACTIVE(LorenzColor.YELLOW),
         NOT_TOUCHING_AIR(LorenzColor.RED),
-        PAUSED(LorenzColor.GREEN),
+        CALM(LorenzColor.GREEN),
     }
 
     private val originalBlocks = mutableMapOf<LorenzVec, Block>()
 
-    var selectedSnake: Snake? = null
+    private var selectedSnake: Snake? = null
 
     @HandleEvent
     fun onBlockChange(event: ServerBlockChangeEvent) {
@@ -60,6 +84,9 @@ object LivingCaveSnakeFeatures {
 
         val distance = 5
 
+        // hypixel sends the current block when right click again
+//         if (old == new) return
+
         if (new == Blocks.lapis_block) {
             val snake = snakes.filter { it.blocks.isNotEmpty() && it.blocks.last().distance(location) < distance }
                 .minByOrNull { it.blocks.last().distance(location) }
@@ -74,33 +101,19 @@ object LivingCaveSnakeFeatures {
         for (snake in snakes) {
             if (location in snake.blocks) {
                 if (originalBlocks[location] == new || new == Blocks.lapis_ore) {
+//                 if (originalBlocks[location] == new) {
+                    originalBlocks.remove(location)
                     snake.blocks = snake.blocks.editCopy { remove(location) }
                     if (snake.blocks.isEmpty()) {
                         snakes = snakes.editCopy { remove(snake) }
                     }
-                    if (snake.phase == Phase.SPAWNING) {
-                        snake.phase = Phase.MOVING
+                    if (snake.state == State.SPAWNING) {
+                        snake.state = State.ACTIVE
                     }
                     snake.lastRemoveTime = SimpleTimeMark.now()
                 }
             }
         }
-    }
-
-    private fun Duration.getType(): DiffType {
-        val ms = inWholeMilliseconds
-        return when {
-            ms < 50 -> DiffType.SMALL
-            ms in 100..200 -> DiffType.BIG
-
-            else -> DiffType.UNKNWON
-        }
-    }
-
-    enum class DiffType {
-        BIG,
-        SMALL,
-        UNKNWON
     }
 
     private fun getClosest(): Snake? {
@@ -113,12 +126,27 @@ object LivingCaveSnakeFeatures {
         return snakeDistances.sorted().keys.first()
     }
 
+    // TODO maybe move this in repo
+    private val pickaxes = setOf(
+        "SELF_RECURSIVE_PICKAXE",
+        "ANTI_SENTIENT_PICKAXE",
+        "EON_PICKAXE",
+        "CHRONO_PICKAXE",
+    ).map { it.toInternalName() }
+
     @HandleEvent
     fun onBlockClick(event: BlockClickEvent) {
         if (!isEnabled()) return
-        getClosest()?.let {
-            if (event.position in it.blocks) {
-                selectedSnake = it
+        val snake = getClosest() ?: return
+        if (event.position !in snake.blocks) return
+
+        selectedSnake = snake
+        if (event.clickType == ClickType.RIGHT_CLICK) {
+            if (InventoryUtils.itemInHandId == "FROZEN_WATER_PUNGI".toInternalName())
+                snake.lastCalmTime = SimpleTimeMark.now()
+        } else {
+            if (InventoryUtils.itemInHandId in pickaxes) {
+                snake.lastBreakTime = SimpleTimeMark.now()
             }
         }
     }
@@ -129,22 +157,16 @@ object LivingCaveSnakeFeatures {
 
         snakes = snakes.filter { !it.invalidSize() }
         for (snake in snakes) {
-            if (snake.phase == Phase.SPAWNING) continue
+            if (snake.state == State.SPAWNING) continue
 
-            snake.phase = if (snake.isBelowBlocks()) {
-                Phase.NOT_TOUCHING_AIR
+            snake.state = if (snake.isNotTouchingAir()) {
+                State.NOT_TOUCHING_AIR
             } else {
-                val paused = snake.lastAddTime.passedSince() > 200.milliseconds
-                if (paused) Phase.PAUSED else Phase.MOVING
+                val notMoving = snake.lastAddTime.passedSince() > 200.milliseconds
+                if (notMoving) State.CALM else State.ACTIVE
             }
         }
     }
-
-    private fun Snake.invalidSize(): Boolean = blocks.zipWithNext().any { (a, b) ->
-        a.distance(b) > 3
-    }
-
-    private fun Snake.isBelowBlocks(): Boolean = blocks.any { it.isBelowBlocks() }
 
     private val directions = setOf(
         LorenzVec(1, 0, 0),
@@ -155,7 +177,7 @@ object LivingCaveSnakeFeatures {
         LorenzVec(0, 0, -1),
     )
 
-    private fun LorenzVec.isBelowBlocks(): Boolean = directions.none { plus(it).getBlockAt() == Blocks.air }
+    private fun LorenzVec.isNotTouchingAir(): Boolean = directions.none { plus(it).getBlockAt() == Blocks.air }
 
     @HandleEvent
     fun onRenderWorld(event: SkyHanniRenderWorldEvent) {
@@ -166,28 +188,39 @@ object LivingCaveSnakeFeatures {
         for (snake in snakes) {
             val blocks = snake.blocks
             if (blocks.isEmpty()) continue
-            val color = snake.phase.color.toColor()
-            if (blocks.size > 1 && snake.phase == Phase.PAUSED) {
+            val color = snake.state.color.toColor()
+
+            val interaction = snake.getInteraction()
+
+            if (blocks.size > 1 && snake.state == State.CALM && interaction != Interaction.CALMING) {
                 blocks.first().let {
                     event.drawWaypointFilled(it, LorenzColor.GREEN.toColor(), seeThroughBlocks)
                     event.drawString(it.add(0.5, 0.5, 0.5), "§aTail", seeThroughBlocks)
                 }
-                if (blocks.size > 2 && snake == selectedSnake) {
+                if (blocks.size > 2 && snake == selectedSnake && interaction == Interaction.BREAKING) {
                     blocks[1].let {
                         event.drawWaypointFilled(it, LorenzColor.GOLD.toColor(), seeThroughBlocks)
                         event.drawString(it.add(0.5, 0.5, 0.5), "§aNext", seeThroughBlocks)
                     }
                 }
             }
-            blocks.last().let {
-                event.drawWaypointFilled(it, color, seeThroughBlocks)
-                val headColor = if (snake.phase == Phase.NOT_TOUCHING_AIR) "§c" else "§e"
-                event.drawString(it.add(0.5, 0.5, 0.5), "${headColor}Head", seeThroughBlocks)
+            if (interaction != Interaction.BREAKING || blocks.size == 1) {
+                blocks.last().let {
+                    event.drawWaypointFilled(it, color, seeThroughBlocks)
+                    val headColor = if (snake.state == State.NOT_TOUCHING_AIR) "§c" else "§e"
+                    event.drawString(it.add(0.5, 0.5, 0.5), "${headColor}Head", seeThroughBlocks)
+                }
             }
             for ((a, b) in blocks.zipWithNext()) {
                 event.draw3DLine(a.add(0.5, 0.5, 0.5), b.add(0.5, 0.5, 0.5), color, 3, !seeThroughBlocks)
             }
         }
+    }
+
+    enum class Interaction {
+        NONE,
+        CALMING,
+        BREAKING,
     }
 
     fun isEnabled() = RiftApi.inRift() && (RiftApi.inLivingCave() || RiftApi.inLivingStillness()) && config.enabled
