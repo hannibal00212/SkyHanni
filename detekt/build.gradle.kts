0695e6b8963ecddc2d86436f1bb0ed94e31f7d0f
Cal <cwolfson58@gmail.com>
2025-01-26 14:50:09 +1100

    don't run detekt on itself

--- detekt/build.gradle.kts
+++ detekt/build.gradle.kts
@@ -1,3 +1,5 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 plugins {
     kotlin("jvm")
     id("com.google.devtools.ksp")
@@ -12,4 +14,8 @@ dependencies {
     testImplementation("io.kotest:kotest-assertions-core:5.9.1")
     testImplementation("io.gitlab.arturbosch.detekt:detekt-test:1.23.7")
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-rules-ruleauthors:1.23.7")
+}
+
+tasks.withType<Detekt>().configureEach {
+    onlyIf { false }
 }
