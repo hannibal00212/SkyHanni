f9b95b00f02a2e4576ad167ee48c5629cf1cea2d
Chiss5618 <78828070+Chiss5618@users.noreply.github.com>
2025-01-25 20:12:56 -0600

    add cooldown display

--- src/main/java/at/hannibal2/skyhanni/features/garden/pests/PestSpawnTimer.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/pests/PestSpawnTimer.kt
@@ -1,20 +1,81 @@
 package at.hannibal2.skyhanni.features.garden.pests
 
 import at.hannibal2.skyhanni.api.event.HandleEvent
+import at.hannibal2.skyhanni.data.model.TabWidget
 import at.hannibal2.skyhanni.events.GuiRenderEvent
+import at.hannibal2.skyhanni.events.WidgetUpdateEvent
 import at.hannibal2.skyhanni.events.garden.pests.PestSpawnEvent
 import at.hannibal2.skyhanni.features.garden.GardenAPI
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
-import at.hannibal2.skyhanni.utils.RenderUtils.renderString
+import at.hannibal2.skyhanni.utils.RegexUtils.firstMatcher
+import at.hannibal2.skyhanni.utils.RenderUtils.renderRenderables
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import at.hannibal2.skyhanni.utils.TimeUtils.format
+import at.hannibal2.skyhanni.utils.renderables.Renderable
+import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
 
 @SkyHanniModule
 object PestSpawnTimer {
 
     private val config get() = PestAPI.config.pestTimer
 
+    private val patternGroup = RepoPattern.group("garden.pests")
+
+    /**
+     * REGEX-TEST:  Cooldown: §r§a§lREADY
+     * REGEX-TEST:  Cooldown: §r§e1m 58s
+     * REGEX-TEST:  Cooldown: §r§e1m
+     * REGEX-TEST:  Cooldown: §r§e58s
+     * REGEX-TEST:  Cooldown: §r§c§lMAX PESTS
+     */
+
+    private val pestCooldownPattern by patternGroup.pattern(
+        "cooldown",
+        "\\sCooldown: §r§.(?:§.)?(?<minutes>\\d+m)? ?(?<seconds>\\d+s)?(?<ready>READY)?(?<maxPests>MAX PESTS)?",
+    )
+
     var lastSpawnTime = SimpleTimeMark.farPast()
+
+    private var pestCooldownEndTime: SimpleTimeMark? = null
+
+    @HandleEvent
+    fun onWidgetUpdate(event: WidgetUpdateEvent) {
+        if (!event.isWidget(TabWidget.PESTS)) return
+        pestCooldownPattern.firstMatcher(event.widget.lines) {
+            val minutes = group("minutes")?.toInt()
+            val seconds = group("seconds")?.toInt()
+            val ready = group("cooldown") != null
+            val maxPests = group("maxPests") != null
+
+            if (ready) {
+                pestCooldownEndTime = SimpleTimeMark.farPast()
+                return
+            }
+
+            if (maxPests) return
+
+            if (minutes == null && seconds == null) return
+
+            val tablistCooldownEnd = SimpleTimeMark.now() + (minutes?.minutes ?: 0.seconds) + (seconds?.seconds ?: 0.seconds)
+
+            if (pestCooldownEndTime == null) {
+                pestCooldownEndTime = tablistCooldownEnd
+            }
+
+            if (shouldSetCooldown(tablistCooldownEnd, minutes, seconds)) {
+
+                // hypixel generally rounds times down to nearest min if over 6 mins, we'll overestimate and add a min
+
+                pestCooldownEndTime = if (seconds == null && (minutes ?: 0) >= 6) {
+                    tablistCooldownEnd + 1.minutes
+                } else {
+                    tablistCooldownEnd
+                }
+            }
+        }
+    }
 
     @HandleEvent
     fun onPestSpawn(event: PestSpawnEvent) {
@@ -26,14 +87,43 @@ object PestSpawnTimer {
         if (!isEnabled()) return
         if (config.onlyWithVacuum && !PestAPI.hasVacuumInHand()) return
 
-        val display = if (lastSpawnTime.isFarPast()) {
+        val display = drawDisplay()
+
+        config.position.renderRenderables(display, posLabel = "Pest Spawn Timer")
+    }
+
+    private fun shouldSetCooldown(tabCooldownEnd: SimpleTimeMark, minutes: Int?, seconds: Int?): Boolean {
+
+        // tablist can have up to 6 seconds of delay, besides this, there is no scenario where tablist will overestimate cooldown
+        if (tabCooldownEnd > ((pestCooldownEndTime ?: SimpleTimeMark.now()) + 6.seconds)) return true
+
+        // tablist sometimes rounds down to nearest min
+        if ((tabCooldownEnd + 1.minutes) < (pestCooldownEndTime ?: SimpleTimeMark.now()) && seconds == null) return true
+
+        // tablist shouldn't underestimate if it is displaying seconds
+        if ((tabCooldownEnd + 1.seconds) < (pestCooldownEndTime ?: SimpleTimeMark.now()) && seconds != null) return true
+
+        return false
+    }
+
+    private fun drawDisplay() = buildList {
+        val lastPestSpawned = if (lastSpawnTime.isFarPast()) {
             "§cNo pest spawned since joining."
         } else {
             val timeSinceLastPest = lastSpawnTime.passedSince().format()
             "§eLast pest spawned §b$timeSinceLastPest ago"
         }
 
-        config.position.renderString(display, posLabel = "Pest Spawn Timer")
+        add(Renderable.string(lastPestSpawned))
+
+        val pestCooldown = if (!TabWidget.PESTS.isActive) {
+            "§cPests Widget not detected! Enable via /widget!"
+        } else {
+            val cooldownValue = pestCooldownEndTime?.passedSince()?.format() ?: "§cUnknown"
+            "§ePest Cooldown: §b$cooldownValue"
+        }
+
+        add(Renderable.string(pestCooldown))
     }
 
     fun isEnabled() = GardenAPI.inGarden() && config.enabled
