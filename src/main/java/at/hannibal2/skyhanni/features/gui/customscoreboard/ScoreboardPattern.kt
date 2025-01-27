d7155529eea4ac5235abb394071625273e2533a8
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-01-27 20:55:46 +0100

    thingy
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/ScoreboardPattern.kt
+++ src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/ScoreboardPattern.kt
@@ -666,10 +666,10 @@ object ScoreboardPattern {
      * REGEX-TEST: §eBring items to Moby
      * REGEX-TEST:  Glowing Mushroom §8x8
      */
-    @Suppress("LongLine")
+    @Suppress("MaxLineLength")
     val wtfAreThoseLinesPattern by miscSb.pattern(
         "wtfarethoselines",
-        "§eMine \\d+ .*|§eKill 100 Automatons|§eFind a Jungle Key|§eFind the \\d+ Missing Pieces?|§eTalk to the Goblin King|§eBring items to Moby| §.Glowing Mushroom §8x\\d",
+        "§eMine \\d+ .*|§eKill 100 Automatons|§eFind a Jungle Key|§eFind the \\d+ Missing Pieces?|§eTalk to the Goblin King|§eBring items to Moby| Glowing Mushroom §8x\\d",
     )
     val darkAuctionCurrentItemPattern by miscSb.pattern(
         "darkauction.currentitem",
