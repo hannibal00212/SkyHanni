4fdf281a36cfe2ee40b219e78a56cb9a37cbb3e6
YoGoUrT <91050199+YoGoUrT20@users.noreply.github.com>
2025-01-28 21:41:03 +0100

    Update src/main/java/at/hannibal2/skyhanni/utils/ItemPriceUtils.kt
    Co-authored-by: Luna <lunaynx@gmail.com>

--- src/main/java/at/hannibal2/skyhanni/utils/ItemPriceUtils.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/ItemPriceUtils.kt
@@ -60,11 +60,8 @@ object ItemPriceUtils {
             return 7.0 // NPC price
         }
 
-        return if (priceSource == ItemPriceSource.NPC_SELL) {
-            getNpcPriceOrNull()
-        } else {
-            getNpcPriceOrNull() ?: getRawCraftCostOrNull(priceSource, pastRecipes)
-        }
+        return getNpcPriceOrNull()
+            ?: getRawCraftCostOrNull(priceSource, pastRecipes).takeUnless { priceSource == ItemPriceSource.NPC_SELL }
     }
 
     fun NeuInternalName.isAuctionHouseItem(): Boolean = getLowestBinOrNull() != null
