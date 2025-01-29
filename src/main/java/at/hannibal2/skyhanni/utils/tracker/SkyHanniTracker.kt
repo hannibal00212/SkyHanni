a5ac42da41f1d426b3ee37416c2d77ef6829e83c
David Cole <40234707+DavidArthurCole@users.noreply.github.com>
2025-01-28 22:05:36 -0500

    Detekt

--- src/main/java/at/hannibal2/skyhanni/utils/tracker/SkyHanniTracker.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/tracker/SkyHanniTracker.kt
@@ -12,8 +12,8 @@ import at.hannibal2.skyhanni.test.command.ErrorManager
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.CollectionUtils
 import at.hannibal2.skyhanni.utils.CollectionUtils.addSearchableSelector
-import at.hannibal2.skyhanni.utils.ItemPriceSource
 import at.hannibal2.skyhanni.utils.InventoryDetector
+import at.hannibal2.skyhanni.utils.ItemPriceSource
 import at.hannibal2.skyhanni.utils.ItemPriceUtils.getPrice
 import at.hannibal2.skyhanni.utils.LorenzUtils
 import at.hannibal2.skyhanni.utils.NeuInternalName
