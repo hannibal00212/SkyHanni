9b1ce7e106d4765797cad76bcf2f57683616755a
Tryp0xd <54819490+Tryp0xd@users.noreply.github.com>
2025-01-31 16:04:44 +0000

    detekt pls :pleading_face:

--- src/main/java/at/hannibal2/skyhanni/config/features/itemability/CrownOfAvariceConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/itemability/CrownOfAvariceConfig.kt
@@ -4,9 +4,9 @@ import at.hannibal2.skyhanni.config.FeatureToggle
 import at.hannibal2.skyhanni.config.core.config.Position
 import com.google.gson.annotations.Expose
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorSlider
 import io.github.notenoughupdates.moulconfig.annotations.ConfigLink
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
-import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorSlider
 
 class CrownOfAvariceConfig {
     @Expose
